e94c8f3ee3b8264db19122e066ea0b5088705941
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-11 02:22:45 +0530

    ADDED Code to return total fare for multiple rides.

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/utility/Ride.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.cabinvoicegenerator.utility;
+
+public class Ride
+{
+    public final double distance;
+    public final int time;
+
+    public Ride(double distance, int time)
+    {
+        this.distance = distance;
+        this.time = time;
+    }
+}
